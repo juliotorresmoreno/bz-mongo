224179685b461fac7cdc4db40591d0b464a064c7
Julio Torres <jtorres@nativapps.co>
2018-01-03 10:29:33 -0500

    add logger

--- build/actions/ping.js
+++ build/actions/ping.js
@@ -4,15 +4,23 @@ var _require = require('child_process'),
     spawn = _require.spawn;
 
 exports.invoke = function (deps, globals, actionName, data, authenticationType, logger, done) {
-    var ls = spawn('mongocli.exe', ['--command=ping', '--globals=' + JSON.stringify(globals), '--actionName=' + actionName, '--data=' + JSON.stringify(data), '--authenticationType=' + authenticationType]);
+    try {
+        logger.info("ping started");
+        var ls = spawn('mongocli.exe', ['--command=ping', '--globals=' + JSON.stringify(globals), '--actionName=' + actionName, '--data=' + JSON.stringify(data), '--authenticationType=' + authenticationType]);
+        logger.info("ping run");
 
-    ls.stdout.on('data', function (data) {
-        done(data.toString("utf-8"));
-    });
+        ls.stdout.on('data', function (data) {
+            logger.info("ping output");
+            done(data.toString("utf-8"));
+        });
 
-    ls.stderr.on('data', function (data) {
-        done(data.toString("utf-8"));
-    });
+        ls.stderr.on('data', function (data) {
+            logger.info("ping error");
+            done(data.toString("utf-8"));
+        });
+    } catch (e) {
+        logger.info(e);
+    }
 }.bind(null, {});
 
 exports.invoke.description = 'The ping command is a no-op used to test whether a server is responding to commands. ' + 'This command will return immediately even if the server is write-locked: { ping: 1 }';
--- src/ping.js
+++ src/ping.js
@@ -2,21 +2,29 @@
 const { spawn } = require('child_process');
 
 exports.invoke = function (deps, globals, actionName, data, authenticationType, logger, done) {
-    const ls = spawn('mongocli.exe', [
-        '--command=ping',
-        '--globals=' + JSON.stringify(globals),
-        '--actionName=' + actionName,
-        '--data=' + JSON.stringify(data),
-        '--authenticationType=' + authenticationType
-    ]);
+    try {
+        logger.info("ping started");
+        const ls = spawn('mongocli.exe', [
+            '--command=ping',
+            '--globals=' + JSON.stringify(globals),
+            '--actionName=' + actionName,
+            '--data=' + JSON.stringify(data),
+            '--authenticationType=' + authenticationType
+        ]);
+        logger.info("ping run");
 
-    ls.stdout.on('data', (data) => {
-        done(data.toString("utf-8"));
-    });
+        ls.stdout.on('data', (data) => {
+            logger.info("ping output");
+            done(data.toString("utf-8"));
+        });
 
-    ls.stderr.on('data', (data) => {
-        done(data.toString("utf-8"));
-    });
+        ls.stderr.on('data', (data) => {
+            logger.info("ping error");
+            done(data.toString("utf-8"));
+        });
+    } catch (e) {
+        logger.info(e);
+    }
 }.bind(null, {});
 
 exports.invoke.description = `The ping command is a no-op used to test whether a server is responding to commands. ` +
